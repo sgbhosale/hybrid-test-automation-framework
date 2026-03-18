9fdcc74e74b5e23129b551499b188d6d065c7177
unknown <you@example.com>
2026-03-18 09:01:08 +0530

    Changed to headless

--- src/main/java/base/UiTestBase.java
+++ src/main/java/base/UiTestBase.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -23,7 +24,9 @@ public class UiTestBase {
 			DriverManager.setDriver(webdriver);
 		}
 		else if(browser.equalsIgnoreCase("firefox")) {
-			DriverManager.setDriver(new FirefoxDriver());
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+		    firefoxOptions.addArguments("--headless");
+			DriverManager.setDriver(new FirefoxDriver(firefoxOptions));
 		}
 		DriverManager.getDriver().manage().window().maximize();
 		DriverManager.getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
